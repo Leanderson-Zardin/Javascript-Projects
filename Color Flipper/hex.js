fec7806ea3d8b739fc1cf87096e9699d705fe5d5
Leanderson Zardin <leandersonzardin1997@gmail.com>
2024-04-26 22:45:02 -0300

    simple and hex

---
+++ Color Flipper/hex.js
@@ -0,0 +1,20 @@
+let button = document.querySelector("button");
+let cor = document.querySelector("p");
+
+let hex = ["a", "b", "c", "d", "e", "f", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function changeColor() {
+  let random1 = Math.floor(Math.random() * 16)
+  let random2 = Math.floor(Math.random() * 16)
+  let random3 = Math.floor(Math.random() * 16)
+  let random4 = Math.floor(Math.random() * 16)
+  let random5 = Math.floor(Math.random() * 16)
+  let random6 = Math.floor(Math.random() * 16)
+
+  let color = document.querySelector(".main");
+
+  color.style.backgroundColor = `#${hex.random1}+ ${hex.random2}+ ${hex.random3}+ ${hex.random4}+ ${hex.random5}+ ${hex.random6}`;
+  cor.innerText = `#`;
+}
+
+button.addEventListener("click", changeColor)
